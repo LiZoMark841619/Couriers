4634a1531912e62f49bddbeec54a550d640f531d
= <=>
2025-05-22 22:33:23 +0200

    Navbar.tsx added

--- frontend/api.ts
+++ frontend/api.ts
@@ -1,7 +1,8 @@
 import Constants from 'expo-constants';
+import axios from 'axios';
 
 const api = axios.create({
-    baseUrl: Constants.expoConfig?.extra?.API_URL || 'http://localhost:3000',
+    baseURL: Constants.expoConfig?.extra?.API_URL || 'http://192.168.100.9:5050',
     headers: {
         'Content-Type': 'application/json',
     },
