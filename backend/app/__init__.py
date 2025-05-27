4dbc7b8771e87581130855e6f5c2acb95e7b40c1
= <=>
2025-05-27 21:42:30 +0200

    requirements.txt updated, init.py too

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 from app.configs import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     CORS(app, resources={r"/*": {"origins": "*"}})
     db.init_app(app)
+    migrate.init_app(app, db)
     
     from app.models import User
     from app.routes import routes
